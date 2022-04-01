55fd2ad6418baa42ec11f81fe01483d0d73b0a60
Bernd Müller <bernd@muellerbernd.de>
2022-04-01 10:18:24 +0200

    changed shell script

--- setup_links.sh
+++ setup_links.sh
@@ -1,7 +1,7 @@
 mkdir ~/qmk_firmware/keyboards/bernd
-ln -s ~/Desktop/GitProjects/UsefulConfigs/berndsKeyboards/planck/keymaps/bernd/ ~/qmk_firmware/keyboards/planck/keymaps
-ln -s ~/Desktop/GitProjects/UsefulConfigs/berndsKeyboards/myHelidox/ ~/qmk_firmware/keyboards/bernd/
-ln -s ~/Desktop/GitProjects/UsefulConfigs/berndsKeyboards/myKimiko/ ~/qmk_firmware/keyboards/bernd/
-ln -s ~/Desktop/GitProjects/UsefulConfigs/berndsKeyboards/ferris/ ~/qmk_firmware/keyboards/bernd/
-ln -s ~/Desktop/GitProjects/UsefulConfigs/berndsKeyboards/mySweep/ ~/qmk_firmware/keyboards/bernd/
-ln -s ~/Desktop/GitProjects/UsefulConfigs/berndsKeyboards/muellerbernd/ ~/qmk_firmware/users/
+ln -s planck/keymaps/bernd/ ~/qmk_firmware/keyboards/planck/keymaps
+ln -s myHelidox/ ~/qmk_firmware/keyboards/bernd/
+ln -s myKimiko/ ~/qmk_firmware/keyboards/bernd/
+ln -s ferris/ ~/qmk_firmware/keyboards/bernd/
+ln -s mySweep/ ~/qmk_firmware/keyboards/bernd/
+ln -s muellerbernd/ ~/qmk_firmware/users/
